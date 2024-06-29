c07f595d090bb057285b524c1ec2cbe65a4712a8
Marlith08 <150297300+Marlith08@users.noreply.github.com>
2024-06-28 21:52:26 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -4,19 +4,16 @@ import tensorflow as tf
 import numpy as np
 import gdown
 
-
 # Descargar el modelo de Google Drive
 @st.cache_resource
 def load_model():
-    url = 'https://drive.google.com/drive/u/0/folders/1YmhsgmJpdeyDSOZ8I9KqHosJrkyDMfKD'
+    url = 'https://drive.google.com/file/d/1pHQW0c7nauYcO1748kBNyX1nwcmFFx8l/view?usp=sharing'  
     output = 'model.h5'
     gdown.download(url, output, quiet=False)
     model = tf.keras.models.load_model(output)
     return model
 
-
 model = load_model()
-
 
 # Función para procesar la imagen y hacer la predicción
 def predict(image):
@@ -29,10 +26,18 @@ def predict(image):
     predictions = model.predict(image)
     return predictions
 
-
 # Interfaz de usuario con Streamlit
 st.title("Clasificación de Imágenes con Machine Learning")
 st.write("Sube una imagen para obtener una predicción")
 
 # Subir imagen
 uploaded_file = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Imagen subida.', use_column_width=True)
+    st.write("")
+    st.write("Clasificando...")
+
+    predictions = predict(image)
+    st.write("Predicción:", predictions)
