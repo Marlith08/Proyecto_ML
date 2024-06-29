b4b6205dbd8da5c43a8bb5722f60f9ed9c5f00b1
Antony16 <136536376+Sh3ccid@users.noreply.github.com>
2024-06-28 21:56:02 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -8,7 +8,7 @@ import gdown
 @st.cache_resource
 def load_model():
     url = 'https://drive.google.com/file/d/1pHQW0c7nauYcO1748kBNyX1nwcmFFx8l/view?usp=sharing'  
-    output = 'model.h5'
+    output = 'Xception_diabetic_retinopathy_colab_v2.h5'
     gdown.download(url, output, quiet=False)
     model = tf.keras.models.load_model(output)
     return model
