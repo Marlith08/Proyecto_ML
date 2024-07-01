99d11ce49caf105c4046e388397017ffd6d4fd7b
Antony16 <136536376+Sh3ccid@users.noreply.github.com>
2024-06-30 21:47:37 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -12,28 +12,22 @@ uploaded_file = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "pn
 # Verificación de carga de archivo
 if uploaded_file is not None:
     # Ruta completa al modelo
-    modelo_path = 'C:/Users/anton/Xception_diabetic_retinopathy_colab_v2.h5'
+    modelo_path = 'https://github.com/Marlith08/Proyecto_ML/blob/main/.gitattributes'
 
-    try:
-        # Cargar el modelo
-        modelo = load_model(modelo_path)
+    # Cargar el modelo
+    modelo = load_model(modelo_path)
 
-        # Mostrar la imagen subida
-        st.image(uploaded_file, caption='Imagen de entrada', use_column_width=True)
+    # Mostrar la imagen subida
+    st.image(uploaded_file, caption='Imagen de entrada', use_column_width=True)
 
-        # Preprocesamiento de la imagen para hacer la predicción
-        img = image.load_img(uploaded_file, target_size=(224, 224))  # Ajusta según las dimensiones de entrada de tu modelo
-        img_array = image.img_to_array(img)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array /= 255.
+    # Preprocesamiento de la imagen para hacer la predicción
+    img = image.load_img(uploaded_file, target_size=(224, 224))  # Ajusta según las dimensiones de entrada de tu modelo
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.
 
-        # Realizar la predicción
-        prediction = modelo.predict(img_array)
+    # Realizar la predicción
+    prediction = modelo.predict(img_array)
 
-        # Mostrar resultados
-        st.write(f'Predicción: {prediction}')
-
-    except FileNotFoundError:
-        st.error(f'Error: No se pudo encontrar el archivo del modelo en {modelo_path}. Verifica la ruta.')
-    except Exception as e:
-        st.error(f'Error inesperado: {str(e)}')
+    # Mostrar resultados
+    st.write(f'Predicción: {prediction}')
