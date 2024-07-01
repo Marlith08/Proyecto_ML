2e80debf136082c7a415ccd789b7fab9d89fe5fd
Marlith08 <150297300+Marlith08@users.noreply.github.com>
2024-07-01 18:46:28 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -4,10 +4,12 @@ from keras.preprocessing import image
 import numpy as np
 import urllib.request
 import os
+
 # Función para descargar el modelo
 @st.experimental_memo
 def download_model(url, filename):
     urllib.request.urlretrieve(url, filename)
+
 # Descargar el modelo si no está ya en la carpeta
 model_url = 'http://server01.labs.org.pe:2005/Xception_diabetic_retinopathy_colab_v2.h5'
 model_filename = 'Xception_diabetic_retinopathy_colab_v2.h5'
@@ -15,28 +17,33 @@ model_filename = 'Xception_diabetic_retinopathy_colab_v2.h5'
 if not os.path.exists(model_filename):
     with st.spinner('Descargando el modelo...'):
         download_model(model_url, model_filename)
-        st.success('Modelo descargado con éxito!')
+    st.success('Modelo descargado con éxito!')
 
 # Título de la aplicación
 st.title('Predicción de Imágenes con Modelo de Deep Learning')
-	@@ -27,20 +31,23 @@ def download_model(url, filename):
 
 # Verificación de carga de archivo
+uploaded_file = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png"])
+
 if uploaded_file is not None:
-    # Cargar el modelo
-    modelo = load_model(model_filename)
+    try:
+        # Cargar el modelo
+        modelo = load_model(model_filename)
 
-    # Mostrar la imagen subida
-    st.image(uploaded_file, caption='Imagen de entrada', use_column_width=True)
+        # Mostrar la imagen subida
+        st.image(uploaded_file, caption='Imagen de entrada', use_column_width=True)
 
-    # Preprocesamiento de la imagen para hacer la predicción
-    img = image.load_img(uploaded_file, target_size=(229, 229))  # Ajusta según las dimensiones de entrada de tu modelo
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.
+        # Preprocesamiento de la imagen para hacer la predicción
+        img = image.load_img(uploaded_file, target_size=(229, 229))  # Ajusta según las dimensiones de entrada de tu modelo
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.
 
-    # Realizar la predicción
-    prediction = modelo.predict(img_array)
+        # Realizar la predicción
+        prediction = modelo.predict(img_array)
 
-    # Mostrar resultados
-    st.write(f'Predicción: {prediction}')
+        # Mostrar resultados
+        st.write(f'Predicción: {prediction}')
+    
+    except Exception as e:
+        st.error(f'Error: {e}')
